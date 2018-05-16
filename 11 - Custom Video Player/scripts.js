2f55e6515fd0deee2abe977c1cef8bbd866500d8
Shaws <seanrichardparker@gmail.com>
2018-05-16 07:09:49 -0400

    handleRangeUpdate (volume and playback speed)

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,7 +5,7 @@ const progress = document.querySelector('.progress');
 const progressBar = document.querySelector('.progress__filled');
 const toggle = document.querySelector('.toggle');
 const skipButtons = document.querySelectorAll('[data-skip]');
-const ranges = document.querySelectorAll('player__slider');
+const ranges = document.querySelectorAll('.player__slider');
 
 /* Build out functions */
 function togglePlay() {
@@ -30,6 +30,12 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  // console.log(this.name);
+  // console.log(this.value);
+}
+
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -37,3 +43,5 @@ video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
